fe4ce5f5297f9d66b3fdfaa7765715b7375bef75
Sumant Dey <sumant.dey@edgeverve.com>
2018-05-02 13:34:32 +0530

    Initial spring commit

---
+++ src/main/java/org/fuber/cabservice/fubercabservice/FuberCabserviceApplication.java
@@ -0,0 +1,12 @@
+package org.fuber.cabservice.fubercabservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuberCabserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuberCabserviceApplication.class, args);
+	}
+}
